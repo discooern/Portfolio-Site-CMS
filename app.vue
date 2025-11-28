5b34cf50cf68b8b9fc6f8a33f1a874df29dff347
jonas-mortensen-01 <jonas.mortensen@specialisterne.com>
2025-11-28 13:14:49 +0100

    - Initial Commit

---
+++ app.vue
@@ -0,0 +1,30 @@
+<template>
+  <body>
+    <div class="flex flex-col justify-between">
+      <PageHeader>
+        
+      </PageHeader>
+      <main class="max-w-[80vw] mx-auto">
+        <NuxtLayout>
+          <NuxtPage>
+
+          </NuxtPage>
+        </NuxtLayout>
+      </main>
+      <PageFooter>
+
+      </PageFooter>
+    </div>
+  </body>
+</template>
+
+<script lang="ts">
+// Components
+import PageHeader from './components/PageHeader.vue';
+import PageFooter from './components/PageFooter.vue';
+
+export default defineComponent({
+  name: 'HomePage',
+  components: { PageHeader, PageFooter },
+})
+</script>
